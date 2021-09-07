015fb7152b34764cdcd932fbcd1ca26d1684b3bd
Vishal Solanki <76219431+VishalSolanki135@users.noreply.github.com>
2021-09-07 07:54:56 +0530

    Added the Loader in the main Activity.
    To handle the background async task.

--- QuakeReport/app/src/main/java/com/example/quakereport/EarthQuakeActivity.java
+++ QuakeReport/app/src/main/java/com/example/quakereport/EarthQuakeActivity.java
@@ -1,7 +1,14 @@
 package com.example.quakereport;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.loader.app.LoaderManager;
+import androidx.loader.content.AsyncTaskLoader;
+import androidx.loader.content.Loader;
 
+import android.annotation.SuppressLint;
+import android.content.Context;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.AsyncTask;
@@ -10,16 +17,19 @@ import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.TextView;
 
+import java.net.URL;
 import java.util.ArrayList;
 import java.util.List;
 
-public class EarthQuakeActivity extends AppCompatActivity {
+public class EarthQuakeActivity extends AppCompatActivity implements LoaderManager.LoaderCallbacks<List<Earthquake>> {
 
     public static final String LOG_TAG=EarthQuakeActivity.class.getName();
     private static final String USGS_REQUEST_URL =
             "https://earthquake.usgs.gov/fdsnws/event/1/query?format=geojson&orderby=time&minmag=3&limit=30";
     private EarthquakeAdapter mAdapter;
+    private TextView mEmptyStateView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -42,28 +52,49 @@ public class EarthQuakeActivity extends AppCompatActivity {
             }
         });
 
-        EarthQuakeAsyncTask task = new EarthQuakeAsyncTask();
-        task.execute(USGS_REQUEST_URL);
+        Bundle queryBundle = new Bundle();
+        queryBundle.putString("url", USGS_REQUEST_URL);
+        getSupportLoaderManager().initLoader(0, queryBundle, this).forceLoad();
+
+        mEmptyStateView = (TextView)findViewById(R.id.empty_view);
+        earthquakeListView.setEmptyView(mEmptyStateView);
     }
 
-    private class EarthQuakeAsyncTask extends AsyncTask<String, Void, List<Earthquake>> {
 
-        @Override
-        protected List<Earthquake> doInBackground(String... urls) {
-            if(urls.length<1 || urls[0]==null) {
-                return null;
-            }
-            List<Earthquake> result = QueryUtils.fetchQueryData(urls[0]);
-            return result;
+    //These all methods are the signs that these will perform an asynchronous behaviour.
+    @Override
+    public Loader<List<Earthquake>> onCreateLoader(int id, Bundle args) {
+        return new EarthquakeLoader(this);//created a async task Loader
+    }//this is called when we need to create and return a new loader object
+
+    @SuppressLint("SetTextI18n")
+    @Override
+    public void onLoadFinished( Loader<List<Earthquake>> loader, List<Earthquake> data) {
+        View loadingIndicator = findViewById(R.id.loading_indicator);
+        loadingIndicator.setVisibility(View.GONE);
+
+        mEmptyStateView.setText(R.string.no_earthquakes);
+        mAdapter.clear();
+        if(data!=null && !data.isEmpty()) {
+//            mAdapter.addAll(data);
+        }
+    }//this is called when loader is finished loading the data in the background thread.
+
+    @Override
+    public void onLoaderReset( Loader<List<Earthquake>> loader) {
+        mAdapter.addAll(new ArrayList<>());
+    }//this is called when the previous created loader is reset.(means data from the last loader is now invalid)
+
+    private static class EarthquakeLoader extends AsyncTaskLoader<List<Earthquake>> {
+        public EarthquakeLoader(Context context) {
+            super(context);
         }
 
-        //jo iska return hoga wo is neeche wale post execute method ke liye input hoga!!!
+
         @Override
-        protected void onPostExecute(List<Earthquake> earthquakes) {
-            mAdapter.clear();
-            if(earthquakes!=null && !earthquakes.isEmpty()) {
-                mAdapter.addAll(earthquakes);
-            }
+        public List<Earthquake> loadInBackground() {
+            List<Earthquake> result = QueryUtils.fetchQueryData(USGS_REQUEST_URL);
+            return result;
         }
     }
 }
